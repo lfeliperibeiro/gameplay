ffbffdfec5ad60393c3d890ec2a27c8b616d37a6
Felipe Ribeiro <piporibeiro@gmail.com>
2021-06-24 00:59:26 -0300

    feat: create ButtonIcon component

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import DiscordImg from '../../assets/discord.png';
-import {Image, Text, TouchableOpacity, View} from "react-native";
+import {RectButton, RectButtonProps} from 'react-native-gesture-handler';
+import {Image, Text, View} from "react-native";
 import {styles} from "./styles";
 
-type ButtonIconProps = {
+type ButtonIconProps = RectButtonProps & {
   title: string;
 }
 
-export function ButtonIcon({title}: ButtonIconProps) {
+export function ButtonIcon({title, ...rest}: ButtonIconProps) {
   return (
 
-      <TouchableOpacity style={styles.container} activeOpacity={.8}>
+      <RectButton
+          style={styles.container}
+          {...rest}
+      >
         <View style={styles.iconWrapper}>
           <Image source={DiscordImg} style={styles.icon}/>
         </View>
         <Text style={styles.title}>
           {title}
         </Text>
-      </TouchableOpacity>
+      </RectButton>
 
   )
 }
