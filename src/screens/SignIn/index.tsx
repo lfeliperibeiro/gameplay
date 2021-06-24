49d1c91a7c27b113e6754e5b3612a375365a46f9
Felipe Ribeiro <piporibeiro@gmail.com>
2021-06-24 00:58:19 -0300

    feat: add navigation with SignIn

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
-import {Image, StatusBar, Text, View} from "react-native";
+import {Image, Text, View} from "react-native";
 import {styles} from './styles'
 import IllustrationImg from '../../assets/illustration.png';
 import {ButtonIcon} from "../../components/ButtonIcon";
+import {useNavigation} from "@react-navigation/native";
 
 export function SignIn() {
+    const navigation = useNavigation()
+    function handleSignIn() {
+        navigation.navigate('Home');
+
+    }
   return (
     <View style={styles.container}>
-      <StatusBar barStyle={'light-content'} backgroundColor={'transparent'} translucent />
       <Image source={IllustrationImg} style={styles.image} resizeMode={'stretch'}/>
       <View style={styles.content}>
         <Text style={styles.title}>
-          Organize {`\n`} suas jogatinas {`\n`} facilmente
+          Conecte-se {`\n`} e ornanize suas {`\n`} jogatinas
         </Text>
         <Text style={styles.subtitle}>
           Crie grupos para jogar seus games {`\n`} favoritos com seus amigos
         </Text>
-          <ButtonIcon title={'Entrar com Discord'}/>
+          <ButtonIcon
+              title={'Entrar com Discord'}
+              onPress={handleSignIn}
+          />
       </View>
     </View>
   )
