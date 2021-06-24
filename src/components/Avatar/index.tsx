3acc6442ed0801d2e2893fc27edfa299281ea628
Felipe Ribeiro <piporibeiro@gmail.com>
2021-06-24 00:59:00 -0300

    feat: create Avatar component

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {Image} from "react-native";
+import {styles} from "./styles";
+import {LinearGradient} from "expo-linear-gradient";
+import {theme} from "../../global/styles/theme";
+
+type AvatarProps = {
+    urlImage: string;
+}
+
+export function Avatar({urlImage}: AvatarProps) {
+    return (
+      <LinearGradient
+          style={styles.container}
+          colors={[theme.colors.secondary50, theme.colors.secondary70]}
+      >
+         <Image
+             source={{uri: urlImage}}
+             style={styles.avatar}
+         />
+      </LinearGradient>
+    )
+}
