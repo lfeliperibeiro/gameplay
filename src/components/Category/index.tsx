76a7a059d8042f3a1d88be9acad3008301252705
Felipe Ribeiro <piporibeiro@gmail.com>
2021-06-24 19:42:16 -0300

    style: change colors and gradient

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -9,18 +9,22 @@ import {Text, View} from "react-native";
 type CategoryProps = RectButtonProps & {
     title: string;
     icon: React.FC<SvgProps>;
-    checked?: boolean
+    checked?: boolean;
+    hasCheckBox?: boolean
 }
 
-export function Category({title, icon: Icon, checked = false, ...rest}: CategoryProps) {
+export function Category({title, icon: Icon, checked = false, hasCheckBox= false, ...rest}: CategoryProps) {
     return (
         <RectButton {...rest}>
             <LinearGradient
                 colors={[theme.colors.secondary50, theme.colors.secondary70]}
                 style={styles.container}
             >
-                <View style={[styles.content, {opacity: checked ? 1 : 0.4}]}>
-                    <View style={checked ? styles.checked : styles.check}/>
+                <LinearGradient
+                    style={[styles.content, {opacity: checked ? 1 : 0.5}]}
+                    colors={[checked ? theme.colors.secondary85 : theme.colors.secondary50, theme.colors.secondary40]}
+                >
+                    {hasCheckBox && <View style={checked ? styles.checked : styles.check}/>}
                     <Icon
                         width={48}
                         height={48}
@@ -28,7 +32,7 @@ export function Category({title, icon: Icon, checked = false, ...rest}: Category
                     <Text style={styles.title}>
                         {title}
                     </Text>
-                </View>
+                </LinearGradient>
             </LinearGradient>
         </RectButton>
     )
