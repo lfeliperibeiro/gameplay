550ac0ea27793a920ad640d077b29f1154c8f144
Felipe Ribeiro <piporibeiro@gmail.com>
2021-06-24 00:53:54 -0300

    feat: create Background component

---
+++ src/components/Background/index.tsx
@@ -0,0 +1,19 @@
+import React, {ReactNode} from 'react';
+import {LinearGradient} from 'expo-linear-gradient';
+import {styles} from './styles';
+import { theme } from '../../global/styles/theme';
+
+type BackgroundProps = {
+    children: ReactNode
+}
+
+export function Background({children}: BackgroundProps) {
+    return (
+        <LinearGradient
+            style={styles.container}
+            colors={[theme.colors.secondary80 ,theme.colors.secondary100]}
+        >
+            {children}
+        </LinearGradient>
+    )
+}
