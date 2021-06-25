4874a7ebca6da075be930484ce75077804ea226f
Felipe Ribeiro <piporibeiro@gmail.com>
2021-06-25 20:42:13 -0300

    refactor: remove prop

--- src/components/Appointment/index.tsx
+++ src/components/Appointment/index.tsx
@@ -7,13 +7,8 @@ import { categories } from '../../utils/categories';
 import PlayerSvg from '../../assets/player.svg';
 import CalendarSvg from '../../assets/calendar.svg';
 import { theme } from '../../global/styles/theme';
-
-export type GuildProps = {
-  id: string;
-  name: string;
-  icon: null;
-  owner: boolean;
-};
+import { GuildProps } from '../Guild';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export type AppointmentProps = {
   id: string;
@@ -34,7 +29,12 @@ export function Appointment({ data, ...rest }: Props) {
   return (
     <RectButton {...rest}>
       <View style={styles.container}>
-        <GuildIcon />
+        <LinearGradient
+          style={styles.guildIconContainer}
+          colors={[theme.colors.secondary50, theme.colors.secondary70]}
+        >
+          <GuildIcon />
+        </LinearGradient>
         <View style={styles.content}>
           <View style={styles.header}>
             <Text style={styles.title}>{data.guild.name}</Text>
