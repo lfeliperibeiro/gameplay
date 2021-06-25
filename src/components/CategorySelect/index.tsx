4291e3ac26cfd33b63038eb66bbc2fe741a73982
Felipe Ribeiro <piporibeiro@gmail.com>
2021-06-24 23:45:14 -0300

    feat: add conditional

--- src/components/CategorySelect/index.tsx
+++ src/components/CategorySelect/index.tsx
@@ -7,9 +7,10 @@ import {Category} from "../Category";
 type CategorySelectProps = {
     categorySelected: string;
     setCategory: (categoryId: string) => void
+    hasCheckBox?: boolean
 }
 
-export function CategorySelect({categorySelected, setCategory}: CategorySelectProps) {
+export function CategorySelect({categorySelected, setCategory, hasCheckBox = false}: CategorySelectProps) {
     return(
        <ScrollView
         horizontal
@@ -24,6 +25,7 @@ export function CategorySelect({categorySelected, setCategory}: CategorySelectPr
                    icon={category.icon}
                    checked={category.id === categorySelected}
                    onPress={() => setCategory(category.id)}
+                   hasCheckBox={hasCheckBox}
                />
            ))}
        </ScrollView>
