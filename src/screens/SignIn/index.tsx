6bd2a59133a7d3ad2c25addfb72fde139c9c079c
Felipe Ribeiro <piporibeiro@gmail.com>
2021-06-24 23:47:42 -0300

    fix: add background component for IOS layout

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -4,6 +4,7 @@ import {styles} from './styles'
 import IllustrationImg from '../../assets/illustration.png';
 import {ButtonIcon} from "../../components/ButtonIcon";
 import {useNavigation} from "@react-navigation/native";
+import {Background} from "../../components/Background";
 
 export function SignIn() {
     const navigation = useNavigation()
@@ -12,20 +13,23 @@ export function SignIn() {
 
     }
   return (
-    <View style={styles.container}>
-      <Image source={IllustrationImg} style={styles.image} resizeMode={'stretch'}/>
-      <View style={styles.content}>
-        <Text style={styles.title}>
-          Conecte-se {`\n`} e ornanize suas {`\n`} jogatinas
-        </Text>
-        <Text style={styles.subtitle}>
-          Crie grupos para jogar seus games {`\n`} favoritos com seus amigos
-        </Text>
-          <ButtonIcon
-              title={'Entrar com Discord'}
-              onPress={handleSignIn}
-          />
-      </View>
-    </View>
+      <Background>
+          <View style={styles.container}>
+              <Image source={IllustrationImg} style={styles.image} resizeMode={'stretch'}/>
+                  <View style={styles.content}>
+                        <Text style={styles.title}>
+                          Conecte-se {`\n`} e ornanize suas {`\n`} jogatinas
+                        </Text>
+                        <Text style={styles.subtitle}>
+                          Crie grupos para jogar seus games {`\n`} favoritos com seus amigos
+                        </Text>
+                          <ButtonIcon
+                          title={'Entrar com Discord'}
+                          onPress={handleSignIn}
+                      />
+                  </View>
+            </View>
+      </Background>
+
   )
 }
