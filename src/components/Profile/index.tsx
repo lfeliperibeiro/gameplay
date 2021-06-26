12395e75534fb163ad6be28ab096c02cdb8d72d0
Felipe Ribeiro <piporibeiro@gmail.com>
2021-06-26 20:43:58 -0300

    feat: add signOut condition

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View } from 'react-native';
 import { styles } from './styles';
 import { Avatar } from '../Avatar';
 import { useAuth } from '../../hooks/auth';
+import { RectButton } from 'react-native-gesture-handler';
+import { SignOut } from '../SignOut';
+import { ModalView } from '../ModalView';
 
 export function Profile() {
-  const { user } = useAuth();
+  const [openModal, setOpenModal] = useState(false);
+  const { user, signOut } = useAuth();
+
+  function handleOpenModal() {
+    setOpenModal(true);
+  }
+  function handleCloseModal() {
+    setOpenModal(false);
+  }
   return (
     <View style={styles.container}>
-      <Avatar urlImage={user.avatar} />
+      <RectButton onPress={handleOpenModal}>
+        <Avatar urlImage={user.avatar} />
+      </RectButton>
       <View>
         <View style={styles.user}>
           <Text style={styles.greeting}>Olá,</Text>
@@ -16,6 +29,9 @@ export function Profile() {
         </View>
         <Text style={styles.message}>Hoje é dia de vitória</Text>
       </View>
+      <ModalView visible={openModal} signOut>
+        <SignOut cancel={handleCloseModal} confirm={signOut} />
+      </ModalView>
     </View>
   );
 }
